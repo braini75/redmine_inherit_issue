421ed0ae373c0cb9fb8b6a35983bf3485e825282
Thomas K <braini@sonnenkinder.org>
2016-01-11 14:58:56 +0100

    Some Bugfixes to word with redmine 2.4.2
    issue.show uses <table> with old redmine. Newer versions using <div>.

--- app/controllers/IssueAttributes_controller.rb
+++ app/controllers/IssueAttributes_controller.rb
@@ -1,36 +1,3 @@
 class IssueAttributesController < ApplicationController
-  unloadable
   
-  
-  def get_first_ancestor_with_attribute(issue,custom_field_id)                      
-          if custom_field_id.nil?
-            custom_field_id=(Setting.plugin_redmine_inherit_issue['ancestor_attribute']).to_i
-          end
-          
-          logger.info "Try to find ancestor from: #{issue}"
-          
-          unless issue.parent.nil?
-            idx= issue.parent.editable_custom_fields.find_index{ |custom_field| custom_field.id==custom_field_id}
-  
-            if Setting.plugin_redmine_inherit_issue['ancestor_notset_only']
-              if issue.custom_field_values[idx].value.length > 0
-                return "n.a."
-              end
-            end
-  
-            logger.info "Call------------IDX: #{idx} ----------- VAL: #{issue.parent.custom_field_values[idx].value} ----laenge: #{issue.parent.custom_field_values[idx].value.length}"
-      
-            if issue.parent.custom_field_values[idx].value.length > 0
-              # return first occurence of the attribute
-              return issue.parent.custom_field_values[idx].value
-            else
-              get_first_ancestor_with_attribute(issue.parent,custom_field_id)          
-            end 
-      
-          else
-            # nothing found
-            return "n.a."             
-          end          
-   end
-
 end
