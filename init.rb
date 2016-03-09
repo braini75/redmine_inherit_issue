c909c75ba635fa8eca564a39bbd37b88fd868131
Thomas K <braini@sonnenkinder.org>
2016-03-09 13:26:05 +0100

    small fix: just for nicer view

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_inherit_issue do
   name 'Redmine Inherit Issue Attributes'
   author 'Thomas Koch'
   description 'This is a plugin for Redmine to display Issue Attributes from ancestor Issues.'
-  version '0.5'
+  version '0.6'
   url 'https://github.com/braini75/redmine_inherit_issue'
   author_url 'https://github.com/braini75'  
   
